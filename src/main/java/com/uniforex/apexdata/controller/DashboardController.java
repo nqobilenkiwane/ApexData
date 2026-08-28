9b7bfb9801df1a44106268a002c73a56bb191058
nqobilenkiwane <nqobilenkiwane01@gmail.com>
2026-08-28 21:59:36 +0200

    Remove unsupported Consumer Sentiment endpoint

--- src/main/java/com/uniforex/apexdata/controller/DashboardController.java
+++ src/main/java/com/uniforex/apexdata/controller/DashboardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/dashboard")
+@RequestMapping("/api/dashboard")
 @CrossOrigin(origins = "*")
 public class DashboardController {
 
