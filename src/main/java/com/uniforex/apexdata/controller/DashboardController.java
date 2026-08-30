cb6d1b9270f0ee4ab268dda4f7ab28e0879c8e7c
nqobilenkiwane <nqobilenkiwane01@gmail.com>
2026-08-30 21:40:10 +0200

    Swapped st.loius api calls with Alpha Advantage and Forex Factory api  calls

--- src/main/java/com/uniforex/apexdata/controller/DashboardController.java
+++ src/main/java/com/uniforex/apexdata/controller/DashboardController.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/dashboard")
-@CrossOrigin(origins = "*")
 public class DashboardController {
 
     private final HistoricalScoreRepository historyRepo;
