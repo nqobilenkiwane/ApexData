d6bc8e1dd925e853260288ca8935347bb6c13cc6
nqobilenkiwane <nqobilenkiwane01@gmail.com>
2026-09-03 19:36:29 +0200

    Gonna let things sit for now

--- src/main/java/com/uniforex/apexdata/service/EngineScheduler.java
+++ src/main/java/com/uniforex/apexdata/service/EngineScheduler.java
@@ -116,6 +116,7 @@ public class EngineScheduler {
             System.out.println("[SYSTEM] Fetching Technicals...");
             try {
                 cachedTechData = technicalService.fetchUsdTechnicals();
+                System.out.println("[DEBUG] Calendar Events Found");
             } catch (Exception e) {
                 System.err.println("[API Error] Technicals: " + e.getMessage());
                 System.out.println("[SYSTEM] Rate limit hit. Retaining previously cached Technical Data.");
